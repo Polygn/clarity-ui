9c57d1c74b9927c8dd43d85921da6ce6ebacc0be
Corbin Matschull <Corbin Matschull>
2016-07-19 14:07:45 -0600

    Added new navscreen and nav elements

--- js/app.js
+++ js/app.js
@@ -2,11 +2,12 @@
 //
 //
 //
-(function(){
+$(function(){
 
-        var _contrast = {}
+    $('.app__nav-container').click(function(e){
+        e.preventDefault()
+        $('.app__navscreen-container, .app__nav-bg, .bar').toggleClass('open')
+        $(this).toggleClass('z-index-shift')
+    })
 
-        _contrast.rgbStringToTrip = function(string){
-            
-        }
 })
