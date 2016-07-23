e84d4c1207d96af247c4cf9039c784d00c90dd77
Corbin Matschull <Corbin Matschull>
2016-07-22 22:55:50 -0600

    New nav elems added, and footer added

--- js/app.js
+++ js/app.js
@@ -10,4 +10,22 @@ $(function(){
         $(this).toggleClass('z-index-shift')
     })
 
+    $('.app__user-settingsbox').hover(function(e){
+        e.preventDefault()
+        $('.box').toggleClass('active')
+    })
+
+    $('.app__user-settingsbox').click(function(e){
+        e.preventDefault()
+        $('.app__user-settingspane').toggleClass('settings-open');
+    })
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() + $(this).height() == $(document).height()){
+            $('.app__core-footercontainer').addClass('at--bottom')
+        }else{
+            $('.app__core-footercontainer').removeClass('at--bottom')
+        }
+    })
+
 })
